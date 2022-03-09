e5de2a0d4f0167b7c1311b92ebe1a8a570cd5102
ymyke <michael.naef@gmail.com>
2022-03-09 08:24:49 +0100

    Also test the asset's IRR and the df (at least for type)

--- tests/test_pypme.py
+++ tests/test_pypme.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 from pypme import __version__, calc_pme
 
 
@@ -10,17 +11,22 @@ def test_version():
 
 
 @pytest.mark.parametrize(
-    "cashflows, prices, pme_prices, target_pme_irr",
+    "cashflows, prices, pme_prices, target_pme_irr, target_asset_irr",
     [
         (
             [-100, -50, 60, 100],
             [1.00000000, 1.15000000, 1.28939394, 1.18624242, 1.58165657],
             [100, 105, 115, 100, 120],
             2.02,
+            7.77,
         ),
-        ([-10, 5], [1, 2, 1], [1, 1, 0.5], -25),
+        ([-10, 5], [1, 2, 1], [1, 1, 0.5], -25, 15.14),
     ],
 )
-def test_calc_pme(cashflows, prices, pme_prices, target_pme_irr):
-    pme_irr = calc_pme(cashflows=cashflows, prices=prices, pme_prices=pme_prices)[0]
+def test_calc_pme(cashflows, prices, pme_prices, target_pme_irr, target_asset_irr):
+    pme_irr, asset_irr, df = calc_pme(
+        cashflows=cashflows, prices=prices, pme_prices=pme_prices
+    )
     assert round(pme_irr * 100.0, 2) == round(target_pme_irr, 2)
+    assert round(asset_irr * 100.0, 2) == round(target_asset_irr, 2)
+    assert isinstance(df, pd.DataFrame)
