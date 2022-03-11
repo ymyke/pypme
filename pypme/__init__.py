79d3ebbb950bdaf9df5fd1f01fff89a41f7cc6ba
ymyke <michael.naef@gmail.com>
2022-03-11 10:29:34 +0100

    Bump version after adding license

--- pypme/__init__.py
+++ pypme/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 from .pme import verbose_pme, pme, verbose_xpme, xpme
--- tests/test_pypme.py
+++ tests/test_pypme.py
@@ -7,7 +7,7 @@ from pypme import __version__, verbose_pme, pme, verbose_xpme, xpme
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 @pytest.mark.parametrize(
