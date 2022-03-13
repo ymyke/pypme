47f46206237e92c242cef8db71e16d3b5a55b051
ymyke <michael.naef@gmail.com>
2022-03-13 09:00:28 +0100

    Add documentation

--- pypme/mod_investpy_pme.py
+++ pypme/mod_investpy_pme.py
@@ -1,5 +1,17 @@
-"""
-Types can be any of stock, etf, fund, crypto.
+"""Calculate PME and get the prices from Investing.com via the `investpy` module.
+
+Important: The Investing API has rate limiting measures in place and will block you if
+you hit the API too often. You will notice by getting 429 errors (or maybe
+also/alternatively 503). Wait roughly 2 seconds between each consecutive call to the API
+via the functions in this module.
+
+Args: 
+- pme_type: One of "stock", "etf", "fund", "crypto", "bond", "index", "certificate".
+  Defaults to "stock".
+- pme_ticker: The ticker symbol/name.
+- pme_country: The ticker's country of residence. Defaults to "united states".
+
+Refer to the `pme` module to understand other arguments and what the functions return.
 """
 
 from typing import List, Tuple
@@ -8,12 +20,9 @@ import pandas as pd
 import investpy
 from .pme import verbose_xpme
 
-# FIXME Add documentation (here and in README)
-
 
 def get_historical_data(ticker: str, type: str, **kwargs) -> pd.DataFrame:
-    """Small wrapper to make the investpy interface accessible in a more unified fashion.
-    """
+    """Small wrapper to make the investpy interface accessible in a more unified fashion."""
     kwargs[type] = ticker
     if type == "crypto" and "country" in kwargs:
         del kwargs["country"]
@@ -28,7 +37,8 @@ def investpy_verbose_pme(
     pme_type: str = "stock",
     pme_country: str = "united states",
 ) -> Tuple[float, float, pd.DataFrame]:
-    """Calculate PME for unevenly spaced / scheduled cashflows and return vebose information. FIXME
+    """Calculate PME return vebose information, retrieving PME price information from
+    Investing.com in real time.
     """
     dates_as_str = [x.strftime("%d/%m/%Y") for x in sorted(dates)]
     pmedf = get_historical_data(
@@ -54,7 +64,8 @@ def investpy_pme(
     pme_type: str = "stock",
     pme_country: str = "united states",
 ) -> Tuple[float, float, pd.DataFrame]:
-    """Calculate PME for unevenly spaced / scheduled cashflows and return the PME IRR only. FIXME
+    """Calculate PME and return the PME IRR only, retrieving PME price information from
+    Investing.com in real time.
     """
     return investpy_verbose_pme(
         dates, cashflows, prices, pme_ticker, pme_type, pme_country
